423204144902785a60dfc32a6228b9869daf4128
developer-jessmeshee <developer.jessmeshee@gmail.com>
2026-01-04 18:46:44 -0600

    Configuraciones iniciales del proyecto

---
+++ src/main/java/com/itesco/pizlntI_itesco/PizlntIItescoApplication.java
@@ -0,0 +1,13 @@
+package com.itesco.pizlntI_itesco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizlntIItescoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizlntIItescoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itesco/pizlntI_itesco/PizlntIItescoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itesco.pizlntI_itesco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizlntIItescoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
